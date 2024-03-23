46ea0d4a6c40454b5fcec19246c8fb61a4daedf3
Diego França <diego.franca92@gmail.com>
2024-03-22 22:29:58 -0300

    Include table and modal to create books and users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
+import { TableComponent } from './components/table/table.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

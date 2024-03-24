fabb2a98f73684c9392c868f871c69c9e864b53e
Diego França <diego.franca92@gmail.com>
2024-03-24 14:16:48 -0300

    include mockup data

---
+++ src/app/db/data.ts
@@ -0,0 +1,74 @@
+// A pessoa escolher o dia pra ver se ta disponivel ou
+// ver de acordo com as que tem disponivel nos dias proximos (2 ou 3 dias a frente)
+// O sistema junta as informações do Agendamento numa
+// Só precisar que a Secretaria consiga visualizar,
+// os horarios e dias das profissionais.
+// E as profissionais porem marcar na agenda os dias que não estariam disponiveis
+
+
+const professionals = [
+  {
+    name: 'Gabi',
+    bio: 'Profissonal de excelencia',
+    services: [
+      {
+        name: 'Cilios',
+        duration: 3000,
+        price: 100,
+        description: '',
+        avaiableDates: [
+          {
+            date: '04/05/2024',
+            available: true,
+            avaiableTimes: [8, 9, 10, 11],
+          },
+          {
+            date: '05/05/2024',
+            available: true,
+            avaiableTimes: [8, 9, 10, 11],
+          },
+        ],
+      },
+      {
+        name: 'SPA',
+        duration: 3000,
+        price: 100,
+        description: '',
+        avaiableDates: [
+          {
+            date: '01/05/2024',
+            available: true,
+            avaiableTimes: [8, 9, 10, 11],
+          },
+          {
+            date: '10/05/2024',
+            available: true,
+            avaiableTimes: [8, 9, 10, 11],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const services = [
+  {
+    name: 'Cilios',
+    duration: 3000,
+    price: 100,
+    description: '',
+    professionals: [
+      {
+        name: 'Gabi',
+        price: 100,
+        avaiableDates: [
+          {
+            date: '2021-01-01',
+            available: true,
+            avaiableTimes: [8, 9, 10, 11],
+          },
+        ],
+      },
+    ],
+  },
+];
